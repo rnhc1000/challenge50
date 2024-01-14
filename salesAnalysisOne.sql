c2f13fad6833cbe93aa4d3d8a6c23133cd9ad41a
Ricardo Ferreira <rnhc1000@hotmail.com>
2024-01-14 16:35:33 -0300

    day14Of14:Leetcode SQL50 ... Joins

---
+++ salesAnalysisOne.sql
@@ -0,0 +1,36 @@
+Write a solution to report the product_name, year, and price for each sale_id in the 
+Sales table.
+Return the resulting table in any order.
+The result format is in the following example.
+Example 1:
+
+Input: 
+Sales table:
++---------+------------+------+----------+-------+
+| sale_id | product_id | year | quantity | price |
++---------+------------+------+----------+-------+ 
+| 1       | 100        | 2008 | 10       | 5000  |
+| 2       | 100        | 2009 | 12       | 5000  |
+| 7       | 200        | 2011 | 15       | 9000  |
++---------+------------+------+----------+-------+
+Product table:
++------------+--------------+
+| product_id | product_name |
++------------+--------------+
+| 100        | Nokia        |
+| 200        | Apple        |
+| 300        | Samsung      |
++------------+--------------+
+Output: 
++--------------+-------+-------+
+| product_name | year  | price |
++--------------+-------+-------+
+| Nokia        | 2008  | 5000  |
+| Nokia        | 2009  | 5000  |
+| Apple        | 2011  | 9000  |
++--------------+-------+-------+
+
+SELECT  Product.product_name, Sales.year, Sales.price from Sales 
+    INNER JOIN
+        Product on sales.product_id = product.product_id 
+            order by sales.year desc;
